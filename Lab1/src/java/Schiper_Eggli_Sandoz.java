74ac7c148eb1643f5028a96268d2fd8c56a84f1c
Jianyu Chen <chenjy0046@gmail.com>
2018-11-17 13:14:48 +0100

    add data and stracture of functions

--- Lab1/src/java/Schiper_Eggli_Sandoz.java
+++ Lab1/src/java/Schiper_Eggli_Sandoz.java
@@ -1,5 +1,30 @@
 package java;
 
-public class Schiper_Eggli_Sandoz {
+import java.rmi.RemoteException;
+import java.util.Map;
+
+public class Schiper_Eggli_Sandoz implements Schiper_Eggli_Sandoz_interface{
     
+    long nodes;
+    Map<Integer, String> port; //map each node with its port number
+
+    public void send(int node, Message message) throws RemoteException{
+
+    }
+
+    public void receive(Message message) throws RemoteException{
+
+    }
+
+    /**
+     * 
+     * @param nodes number of nodes in the system
+     * @param port map each node with its port number
+     */
+    Schiper_Eggli_Sandoz(int nodes, Map<Integer, String> port){
+        this.nodes = nodes;
+        this.port = port;
+    }
+
 }
+
