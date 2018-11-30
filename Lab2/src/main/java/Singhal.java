a8d7da51f23ca2734f3792deb035ff380f95615b
jianyuchen <chenjy0046@gmail.com>
2018-11-30 14:06:46 +0100

    Add pom

--- Lab2/src/main/java/Singhal.java
+++ Lab2/src/main/java/Singhal.java
@@ -43,19 +43,21 @@ public class Singhal extends UnicastRemoteObject implements Singhal_RMI, Runnabl
         this.processNum = processNum;
         this.index = index;
         ///initialize the state array
-        for(int i =0;i<processNum;i++){
-            if(i<=index){
-                if(index == 0){
-                    state_array.add(state.H);
-                }else{
-                    state_array.add(state.R);
-                }
-            }else{
+        if(index == 0){
+            state_array.add(state.H);
+            for(int i=1;i<processNum;i++){
                 state_array.add(state.O);
             }
-
-            request_number.add(0);
+        }else{
+            for(int i=0;i<processNum;i++){
+                if(i<index){
+                    state_array.add(state.R);
+                }else{
+                    state_array.add(state.O);
+                }
+            }
         }
+
     }
 
 
